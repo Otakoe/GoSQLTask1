b3b6657483aca92c40cb0133c29b4bc3a7241d76
Otakoe <vitalylarc@gmail.com>
2020-04-10 03:18:37 +0300

    Заполнение связей

--- populateDB.sql
+++ populateDB.sql
@@ -43,3 +43,76 @@ insert into customers (customer_name,minage,maxage) values
     ('Nolife gamers',15,45),
     ('Designers',18,55),
     ('Workers',18,65)
+
+
+insert into link_developers_skills values
+    (1,4),
+    (2,1),
+    (2,4),
+    (3,3),
+    (3,7),
+    (4,1),
+    (4,5),
+    (5,2),
+    (5,5),
+    (6,5),
+    (6,4),
+    (6,1),
+    (7,6),
+    (7,1),
+    (8,3),
+    (8,7),
+    (8,2),
+    (9,7),
+    (9,5),
+    (9,2)
+
+insert into link_developers_projects values
+    (1,6),
+    (1,5),
+    (2,5),
+    (2,6),
+    (2,1),
+    (3,3),
+    (3,2),
+    (4,7),
+    (4,4),
+    (5,4),
+    (5,1),
+    (6,5),
+    (6,1),
+    (6,4),
+    (7,4),
+    (7,7),
+    (8,1),
+    (8,2),
+    (8,3),
+    (8,7),
+    (9,1),
+    (9,2)
+
+insert into link_companies_projects values
+    (1,2),
+    (2,1),
+    (3,3),
+    (4,4),
+    (5,5),
+    (5,6),
+    (6,7)
+
+insert into link_customers_projects values
+    (1,2),
+    (1,3),
+    (1,7),
+    (2,7),
+    (3,1),
+    (3,2),
+    (3,5),
+    (3,6),
+    (4,1),
+    (4,2),
+    (4,5),
+    (5,3),
+    (5,2),
+    (6,5),
+    (6,6)
