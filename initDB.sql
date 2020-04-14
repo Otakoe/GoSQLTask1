b3b10005404f727736798ed6d54b80122e9ef1af
Otakoe <vitalylarc@gmail.com>
2020-04-14 20:13:22 +0300

    Добавил инициализацию схемы

--- initDB.sql
+++ initDB.sql
@@ -1,3 +1,11 @@
+create schema if not exists gosqltask1
+
+CHARACTER SET = utf8mb4
+COLLATE= utf8mb4_0900_ai_ci
+ DEFAULT ENCRYPTION = 'Y';
+ 
+use gosqltask1;
+
 drop table if exists developers;
 create table developers(
 	id int auto_increment,
